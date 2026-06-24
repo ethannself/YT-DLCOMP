853878efc0c741c33841f266e984a5fd83b36202
Ethan <42685193+ethannself@users.noreply.github.com>
2026-06-23 23:37:00 -0400

    refactor(interface): move implementations to interface.cpp

--- include/interface.hpp
+++ include/interface.hpp
@@ -1,44 +1,13 @@
 #pragma once
-#include <array>
-#include <cpr/api.h>
-#include <cpr/cpr.h>
-#include <cpr/cprtypes.h>
-#include <cstdio>
-#include <cstdlib>
-#include <iostream>
-#include <memory>
-#include <stdexcept>
-#include <stdio.h>
-#include <stdlib.h>
+
 #include <string>
+#include <vector>
 
-std::string executeYtDLPCommand(const char *cmd) {
-  std::array<char, 128> buffer;
-  std::string result;
+struct Entry {
+  std::string link;
+  std::string timestamp;
+};
+extern std::vector<Entry> entries;
 
-#if defined(_WIN32)
-  std::shared_ptr<FILE> pipe(_popen(cmd, "r"), _pclose);
-#else
-  std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
-#endif
-
-  if (!pipe)
-    throw std::runtime_error("popen() failed!");
-
-  while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
-    result += buffer.data();
-  }
-  return result;
-}
-void request_responses(std::string spreadsheetId) {
-  char *apiKey = nullptr;
-  size_t size = 0;
-  std::string range = "Form%20Responses%201!A:Z";
-  if (_dupenv_s(&apiKey, &size, "apiKey") == 0 && apiKey != nullptr) {
-
-    auto response = cpr::Get(cpr::Url{std::format(
-        "https://sheets.googleapis.com/v4/spreadsheets/{}/values/{}?key={}",
-        spreadsheetId, range, apiKey)});
-  }
-  // process json here
-}
+std::string executeYtDLPCommand(const char *cmd);
+void request_responses(std::string spreadsheetId);
---
+++ src/interface.cpp
@@ -0,0 +1,71 @@
+#include "interface.hpp"
+
+#include <array>
+#include <cpr/api.h>
+#include <cpr/cpr.h>
+#include <cpr/cprtypes.h>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <nlohmann/json.hpp>
+#include <stdexcept>
+#include <stdio.h>
+#include <stdlib.h>
+
+std::vector<Entry> entries;
+
+std::string executeYtDLPCommand(const char *cmd) {
+  std::array<char, 128> buffer;
+  std::string result;
+
+#if defined(_WIN32)
+  std::shared_ptr<FILE> pipe(_popen(cmd, "r"), _pclose);
+#else
+  std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
+#endif
+
+  if (!pipe)
+    throw std::runtime_error("popen() failed!");
+
+  while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+    result += buffer.data();
+  }
+  return result;
+}
+void request_responses(std::string spreadsheetId) {
+  // TODO: entering api key in client.
+  char *apiKey = nullptr;
+  size_t size = 0;
+  std::string range = "Form%20Responses%201!A:Z";
+  if (_dupenv_s(&apiKey, &size, "apiKey") == 0 && apiKey != nullptr) {
+
+    auto response = cpr::Get(cpr::Url{std::format(
+        "https://sheets.googleapis.com/v4/spreadsheets/{}/values/{}?key={}",
+        spreadsheetId, range, apiKey)});
+
+    if (response.status_code == 200) {
+      std::cout << "Body: " << response.text << std::endl;
+
+      // process json here
+      auto json = nlohmann::json::parse(response.text);
+
+      auto rows = json["values"];
+      // first row is headers so start at index 1
+      for (int i = 1; i < rows.size(); ++i) {
+        auto row = rows[i];
+        Entry entry = Entry{row[1], row[2]};
+      }
+    } else if (response.status_code == 0) {
+      // status 0 means the request never got through (no internet, bad URL,
+      // etc.)
+      std::cerr << "Request failed: " << response.error.message << std::endl;
+    } else {
+      std::cerr << "HTTP error " << response.status_code << ": "
+                << response.text << std::endl;
+    }
+    // process json here
+  } else {
+    std::cerr << "apiKey environment variable not set!" << std::endl;
+  }
+}
